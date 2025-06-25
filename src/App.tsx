3dcfa5004cc5aee14d1f19fc188cfcc039f9b3d9
Testing Master <info@testingmaster.in>
2025-06-25 12:54:33 +0530

    fixing gh issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { DataProvider } from "./context/DataContext";
 import { ThemeProviderWrapper } from "./components/ThemeProvider";
 import Layout from "./components/Layout";
@@ -26,7 +26,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <DataProvider>
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route path="/" element={<Layout />}>
                 <Route index element={<Dashboard />} />
@@ -41,7 +41,7 @@ const App = () => (
               </Route>
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </DataProvider>
       </TooltipProvider>
     </QueryClientProvider>
